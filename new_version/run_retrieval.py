d755c9280c1129ad187bbf418c2272c0111a9c67
Tyler Fairnington <153495927+TylerFair@users.noreply.github.com>
2025-08-04 12:10:29 -0500

    6) Update run_retrieval to have parametric support.

--- new_version/run_retrieval.py
+++ new_version/run_retrieval.py
@@ -35,18 +35,15 @@ profile_type = 'isothermal'  # 'isothermal', 'parametric'
 
 retriever = CombinedRetriever()
 
-#if profile_type == 'isothermal':
-fit_info = retriever.get_default_fit_info(
-    Rs=Rs, Mp=Mp, Rp=Rp,
-    logZ=None, CO_ratio=None, fit_vmr=True,
-    log_cloudtop_P=3, log_scatt_factor=0., scatt_slope=4, scattering_ref_wavelength=1e-6,  # cloudtop pressure , haze factor, haze slope, haze ref wave
-    n=None, log_k=-np.inf, # no mie scattering
-    T_star=T_star, T_spot=4300, spot_cov_frac=0.05,
-    profile_type=profile_type,
-    T=T_eq
-
-)
-'''
+if profile_type == 'isothermal':
+    fit_info = retriever.get_default_fit_info(
+        Rs=Rs, Mp=Mp, Rp=Rp,
+        logZ=None, CO_ratio=None, fit_vmr=True,
+        log_cloudtop_P=3, log_scatt_factor=0., scatt_slope=4, scattering_ref_wavelength=1e-6,  # cloudtop pressure , haze factor, haze slope, haze ref wave
+        n=None, log_k=-np.inf, # no mie scattering
+        T_star=T_star, T_spot=4300, spot_cov_frac=0.05,
+        profile_type=profile_type,
+        T=T_eq)
 elif profile_type == 'parametric':
     fit_info = retriever.get_default_fit_info(
         Rs=Rs, Mp=Mp, Rp=Rp, 
@@ -60,17 +57,13 @@ elif profile_type == 'parametric':
         alpha1=0.9, 
         alpha2=0.8, 
         log_P2=4,
-        log_P3=6,   
-        #log_ref_pressure=5,
-    )
-'''
+        log_P3=6,   )
 
-fit_info.add_gaussian_fit_param('Mp', 0.013 * M_jup) #0.197 * M_jup, 0.013 * M_jup) # can use this 
+
+fit_info.add_gaussian_fit_param('Mp', 0.013 * M_jup)
 fit_info.add_uniform_fit_param('Rp', 0.85 * Rp, 1.15 * Rp)
-#if profile_type == 'isothermal':
-#    fit_info.add_uniform_fit_param('T', 300, 1500)
-fit_info.add_uniform_fit_param('T', 300, 1500)
-'''
+if profile_type == 'isothermal':
+    fit_info.add_uniform_fit_param('T', 300, 1500)
 elif profile_type == 'parametric':
     fit_info.add_uniform_fit_param('T0', 300, 1500)
     fit_info.add_uniform_fit_param('log_P1', -3, 7)
@@ -78,22 +71,21 @@ elif profile_type == 'parametric':
     fit_info.add_uniform_fit_param('log_P3', 3, 7)
     fit_info.add_uniform_fit_param('alpha1', 0.02, 2)
     fit_info.add_uniform_fit_param('alpha2', 0.02, 2)
-    #fit_info.add_uniform_fit_param('log_ref_pressure', -3, 7)
-'''
+
 fit_info.add_uniform_fit_param("log_cloudtop_P", 0, 8) 
 fit_info.add_uniform_fit_param("log_scatt_factor", -2, 5) # investigate -3, 3; -4, 10
 
 fit_info.add_uniform_fit_param("scatt_slope", 0, 12) # investigate 2, 5; -2, 20
 fit_info.add_uniform_fit_param("T_spot", 0.5 * T_star, 1.5 * T_star)  
 fit_info.add_uniform_fit_param("spot_cov_frac", 0, 0.2)
-fit_info.add_gaussian_fit_param("T_star", 80) #4803, 80)
+fit_info.add_gaussian_fit_param("T_star", 80) 
 
 #fit_info.add_uniform_fit_param("cloud_cov_frac", 0, 1)
 
 # SiO2 doesnt exist 
 #general_gases = ["H2O", "CO", "CO2", "CH4", "HCN", "NH3", "H2S", "Na", "K", "SO2", "SiO","TiO", "VO", "C2H2", "H2-He"]
-#general_gases = ["H2O", "CO2", "SO2", "Na", "K", "CO", "H2S", "CH4", "C2H2", "HCN", "NH3"]
-general_gases = ["H2O", "CO2", "Na", "K", "CO",  "CH4", "HCN", "NH3", "H2-He"] # ACTUALLY SPECIFY BACKGROUND SILLY
+#general_gases = ["H2O", "CO2", "SO2", "Na", "K", "CO", "H2S", "CH4", "C2H2", "HCN", "NH3", "H2-He"]
+general_gases = ["H2O", "CO2", "Na", "K", "CO",  "CH4", "HCN", "NH3", "H2-He"]
 
 all_gases = general_gases 
 
@@ -112,3 +104,4 @@ with open("retrieval_result_hatp18b_2.pkl", "wb") as f:
 plotter = Plotter()
 plotter.plot_retrieval_transit_spectrum(result, prefix="best_fit")
 plotter.plot_retrieval_corner(result, filename="dynesty_corner.png")
+
