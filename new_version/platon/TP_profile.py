86767b224f34dc0d45dde7eef9e280c9e3a60a01
Tyler Fairnington <153495927+TylerFair@users.noreply.github.com>
2025-10-18 14:53:04 -0500

    Refactor temperature profile calculations
    Refactor temperature profile calculations for efficiency and clarity, including caching pressure values and optimizing temperature calculations.

--- new_version/platon/TP_profile.py
+++ new_version/platon/TP_profile.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from . import _cupy_numpy as xp
-expn=xp.scipy.special.expn
+expn = xp.scipy.special.expn
 
 from pkg_resources import resource_filename
 
@@ -8,11 +8,21 @@ from .constants import h, c, k_B, AMU, G
 from .params import NUM_LAYERS, MIN_P, MAX_P
 
 class Profile:
+    _pressure_cache = {}
+    _log_pressure_cache = {}
+    
     def __init__(self):
-        self.pressures = xp.logspace(
+        cache_key = (NUM_LAYERS, MIN_P, MAX_P)
+        if cache_key not in Profile._pressure_cache:
+            Profile._pressure_cache[cache_key] = xp.logspace(
                 xp.log10(MIN_P),
                 xp.log10(MAX_P),
                 NUM_LAYERS)
+            Profile._log_pressure_cache[cache_key] = xp.log10(Profile._pressure_cache[cache_key])
+        
+        self.pressures = Profile._pressure_cache[cache_key]
+        self._log_pressures = Profile._log_pressure_cache[cache_key]
+        self._num_layers = NUM_LAYERS
 
     def get_temperatures(self):
         return xp.cpu(self.temperatures)
@@ -25,94 +35,147 @@ class Profile:
             self.set_isothermal(params_dict["T"])
         elif profile_type == "parametric":
             self.set_parametric(
-                params_dict["T0"], 10**params_dict["log_P1"],
-                params_dict["alpha1"], params_dict["alpha2"],
-                10**params_dict["log_P2"], 10**params_dict["log_P3"])
+                params_dict["T0"], 
+                10**params_dict["log_P1"],
+                params_dict["alpha1"], 
+                params_dict["alpha2"],
+                10**params_dict["log_P2"], 
+                10**params_dict["log_P3"])
         elif profile_type == "radiative_solution":
-            self.set_from_radiative_solution(**params_dict)
+            self.set_from_radiative_solution(
+                params_dict["T_star"],
+                params_dict["Rs"],
+                params_dict["a"],
+                params_dict["Mp"],
+                params_dict["Rp"],
+                params_dict["beta"],
+                params_dict["log_k_th"],
+                params_dict["log_gamma"],
+                params_dict.get("log_gamma2"),
+                params_dict.get("alpha", 0),
+                params_dict.get("T_int", 100)
+            )
         else:
-            assert(False)
+            raise ValueError(f"Unknown profile_type: {profile_type}")
                                         
-        
     def set_from_arrays(self, P_profile, T_profile):
         P_profile = xp.array(P_profile)
         T_profile = xp.array(T_profile)
-        self.temperatures = xp.interp(xp.log10(self.pressures), xp.log10(P_profile), T_profile)
+        log_P_profile = xp.log10(P_profile)
+        self.temperatures = xp.interp(self._log_pressures, log_P_profile, T_profile)
 
     def set_isothermal(self, T_day):
-        self.temperatures = xp.ones(len(self.pressures)) * T_day
+        # Use full instead of ones * value for efficiency
+        self.temperatures = xp.full(self._num_layers, T_day, dtype=xp.float64)
 
     def set_parametric(self, T0, P1, alpha1, alpha2, P2, P3):
-        '''Parametric model from https://arxiv.org/pdf/0910.1347.pdf'''
-        P0 = xp.amin(self.pressures)
-
-        T1 = T0 + xp.power(xp.log10(P1/P0)/alpha1, 2.0)
-        T2 = T1 - xp.power(xp.log10(P1/P2)/alpha2, 2.0)
-        T3 = T2 + xp.power(xp.log10(P3/P2)/alpha2, 2.0) 
-  
-
-        self.temperatures = xp.zeros(len(self.pressures))
-        for i, P in enumerate(self.pressures):
-            if P < P1:
-                self.temperatures[i] = T0 + xp.power(xp.log10(P/P0)/alpha1, 2.0)
-            elif P < P3:
-                self.temperatures[i] = T2 + xp.power(xp.log10(P/P2)/alpha2, 2.0)
-            else:
-                self.temperatures[i] = T3
+        """Vectorized parametric model"""
+        log_P = self._log_pressures
+        log_P0 = xp.log10(xp.amin(self.pressures))
+        log_P1 = xp.log10(P1)
+        log_P2 = xp.log10(P2)
+        log_P3 = xp.log10(P3)
+        
+        # pre-compute temperature boundaries
+        T1 = T0 + ((log_P1 - log_P0) / alpha1) ** 2
+        T2 = T1 - ((log_P1 - log_P2) / alpha2) ** 2
+        T3 = T2 + ((log_P3 - log_P2) / alpha2) ** 2
+        
+        mask_region1 = log_P < log_P1
+        mask_region2 = (log_P >= log_P1) & (log_P < log_P3)
+        mask_region3 = log_P >= log_P3
+        
+        self.temperatures = xp.empty(self._num_layers, dtype=xp.float64)
+        
+        self.temperatures[mask_region1] = T0 + ((log_P[mask_region1] - log_P0) / alpha1) ** 2
+        self.temperatures[mask_region2] = T2 + ((log_P[mask_region2] - log_P2) / alpha2) ** 2
+        self.temperatures[mask_region3] = T3
+        
         return T2, T3
 
-
     def set_from_opacity(self, T_irr, info_dict, visible_cutoff=0.8e-6, T_int=100):
+        """opacity-based temperature profile"""
         wavelengths = xp.array(info_dict["unbinned_wavelengths"])
-        d_lambda = xp.diff(wavelengths)
-        d_lambda = xp.append(d_lambda[0], d_lambda)
-
-        # Convert stellar spectrum from photons/time to energy/time
-        stellar_spectrum = xp.array(info_dict["stellar_spectrum"]) * h * c / wavelengths
-
-        # Convert planetary spectrum from energy/time/wavelength to energy/time
-        planet_spectrum = xp.array(info_dict["planet_spectrum"]) * d_lambda
+        stellar_spectrum = xp.array(info_dict["stellar_spectrum"])
+        planet_spectrum = xp.array(info_dict["planet_spectrum"])
         absorption_coeffs = xp.array(info_dict["absorption_coeff_atm"])
         radii = xp.array(info_dict["radii"])
-
-        # Equation 49 here: https://arxiv.org/pdf/1006.4702.pdf
-        visible = wavelengths < visible_cutoff
-        thermal = wavelengths >= visible_cutoff
-        n = xp.array(info_dict["P_profile"]/k_B/info_dict["T_profile"])
-        intermediate_n = (n[0:-1] + n[1:])/2.0
+        P_profile = xp.array(info_dict["P_profile"])
+        T_profile = xp.array(info_dict["T_profile"])
+        
+        d_lambda = xp.diff(wavelengths)
+        d_lambda = xp.concatenate([d_lambda[:1], d_lambda]) 
+        
+        stellar_spectrum_energy = stellar_spectrum * h * c / wavelengths
+        planet_spectrum_energy = planet_spectrum * d_lambda
+        
+        visible_mask = wavelengths < visible_cutoff
+        thermal_mask = ~visible_mask
+        
+        # number density calculation
+        n = P_profile / (k_B * T_profile)
+        intermediate_n = 0.5 * (n[:-1] + n[1:])
+        
+        #  cross-sections
         sigmas = absorption_coeffs / n[:, xp.newaxis]
-        sigma_v = xp.median(xp.average(sigmas[:, visible], axis=1, weights=stellar_spectrum[visible]))
-        sigma_th = xp.median(xp.average(sigmas[:, thermal], axis=1, weights=planet_spectrum[thermal]))
-
+        
+        # Weighted averages with pre-computed masks
+        sigma_v_weighted = sigmas[:, visible_mask] * stellar_spectrum_energy[visible_mask]
+        sigma_th_weighted = sigmas[:, thermal_mask] * planet_spectrum_energy[thermal_mask]
+        
+        sigma_v = xp.median(sigma_v_weighted.sum(axis=1) / stellar_spectrum_energy[visible_mask].sum())
+        sigma_th = xp.median(sigma_th_weighted.sum(axis=1) / planet_spectrum_energy[thermal_mask].sum())
+        
         gamma = sigma_v / sigma_th
-
+        
+        # Optical depth calculation
         dr = -xp.diff(radii)
         d_taus = sigma_th * intermediate_n * dr
         taus = xp.cumsum(d_taus)
-
-        e2 = expn(2, gamma*taus)
-        T4 = 3.0/4 * T_int**4 * (2.0/3 + taus) + 3.0/4 * T_irr**4 * (2.0/3 + 2.0/3/gamma * (1 + (gamma*taus/2 - 1)*xp.exp(-gamma * taus)) + 2.0*gamma/3 * (1 - taus**2/2) * e2)
+        
+        # Temperature calculation 
+        gamma_taus = gamma * taus
+        exp_term = xp.exp(-gamma_taus)
+        e2 = expn(2, gamma_taus)
+        
+        T4 = (0.75 * T_int**4 * (2.0/3 + taus) + 
+              0.75 * T_irr**4 * (2.0/3 + 
+                                 2.0/(3*gamma) * (1 + (gamma_taus/2 - 1) * exp_term) + 
+                                 2*gamma/3 * (1 - taus**2/2) * e2))
+        
         T = T4 ** 0.25
-        self.temperatures = xp.append(T[0], T)
+        self.temperatures = xp.concatenate([T[:1], T])
 
-    def set_from_radiative_solution(self, T_star, Rs, a, Mp, Rp, beta, log_k_th, log_gamma, log_gamma2=None, alpha=0, T_int=100, **ignored_kwargs):
-        '''From Line et al. 2013: http://adsabs.harvard.edu/abs/2013ApJ...775..137L, Equation 13 - 16'''
-
-        k_th = 10.0**log_k_th
-        gamma = 10.0**log_gamma
-        gamma2 = 10.0**log_gamma2
+    def set_from_radiative_solution(self, T_star, Rs, a, Mp, Rp, beta, log_k_th, 
+                                    log_gamma, log_gamma2=None, alpha=0, T_int=100):
+        """ radiative solution"""
+        k_th = 10.0 ** log_k_th
+        gamma = 10.0 ** log_gamma
+        gamma2 = 10.0 ** log_gamma2 if log_gamma2 is not None else None
         
         g = G * Mp / Rp**2
-        T_eq = beta * xp.sqrt(Rs/(2*a)) * T_star
+        T_eq = beta * xp.sqrt(Rs / (2*a)) * T_star
         taus = k_th * self.pressures / g
-
-        def incoming_stream_contribution(gamma):
-            return 3.0/4 * T_eq**4 * (2.0/3 + 2.0/3/gamma * (1 + (gamma*taus/2 - 1)*xp.exp(-gamma * taus)) + 2.0*gamma/3 * (1 - taus**2/2) * expn(2, gamma*taus))
-
+        
+        T_eq4 = T_eq ** 4
+        T_int4 = T_int ** 4
+        
+        def incoming_stream_contribution(gamma_val):
+            gamma_taus = gamma_val * taus
+            exp_term = xp.exp(-gamma_taus)
+            expn_term = expn(2, gamma_taus)
+            
+            term1 = 2.0/3
+            term2 = 2.0/(3*gamma_val) * (1 + (gamma_taus/2 - 1) * exp_term)
+            term3 = 2*gamma_val/3 * (1 - taus**2/2) * expn_term
+            
+            return 0.75 * T_eq4 * (term1 + term2 + term3)
+        
         e1 = incoming_stream_contribution(gamma)
-        T4 = 3.0/4 * T_int**4 * (2.0/3 + taus) + (1 - alpha) * e1
-
+        T4 = 0.75 * T_int4 * (2.0/3 + taus) + (1 - alpha) * e1
+        
         if gamma2 is not None:
             e2 = incoming_stream_contribution(gamma2)
             T4 += alpha * e2
+            
         self.temperatures = T4 ** 0.25
